05c514bc2229e2c06d59c7c267d30124c1980eb1
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2022-05-30 15:38:47 +0530

    average

--- FunctionTypes.java
+++ FunctionTypes.java
@@ -1,17 +1,82 @@
 public class FunctionTypes {//public class must be defined in its own filename
 	public static void main(String[] args) {
 		System.out.println("Begin with main....");
-		System.out.println("Lunch is over..");
+	
+		Dancer theDancer = new Dancer();
+		theDancer.dance();
+		theDancer.dancing(15);
+		String msg = theDancer.dancingIsDone(40);
+		System.out.println("Ratings : "+msg);
 		
-		InterestCalculator myCalci = new InterestCalculator();
-		myCalci.calculateSimpleInterest();
-		myCalci.calculateSimpleInterest();
-		myCalci.calculateSimpleInterest();
+		int hours= theDancer.relax();
+		System.out.println("Relaxing hours : "+hours);
+		System.out.println("----------------");
+		MyMath m = new MyMath();
+		float a1 = m.calcAverage(45, 23,67);
+		System.out.println("average is "+a1);
 		
-		Boxer boxer  = new Boxer();
-		boxer.fight();		boxer.fight();		boxer.fight();
-		boxer.defend();		boxer.defend();		boxer.fight();
-		boxer.fight();		boxer.knockDown();		
+		 m.calculateAverage(66, 77, 88);
+		
+		
+	}
+}
+class MyMath
+{
+	void calcAvg() {//1
+		int i=10;int j=20; int k=30;
+		float avg= (i+j+k)/3.0f;
+		System.out.println("Avg is : "+avg);
+	}
+	
+	float calcAverage() { //4
+		int i=10;int j=20; int k=30;
+		float avg= (i+j+k)/3.0f;
+		return avg;
+	}
+	
+	float calcAverage(int i,int j, int k) {//3
+		float avg= (i+j+k)/3.0f;
+		return avg;
+	}
+	
+	void calculateAverage(int i,int j, int k) { //2
+		float avg= (i+j+k)/3.0f;
+		System.out.println("average is : "+avg);
+	}
+}
+class Dancer
+{ //time + space + situation
+	
+	void dance() {
+		System.out.println("Dancer is dancing....");
+	}
+	void dancing(int mnts) {
+		System.out.println("Dancer is dancing  for ...."+mnts+" minutes....");
+	}
+	String dancingIsDone(int mnts) {
+		System.out.println("Dancer is dancing  for ...."+mnts+" minutes....");
+		return "Got five star rating... * * * * * ";
+	}
+	int relax() {
+		System.out.println("Dancer is relaxing....");
+		return 6; //returning 6 as a relaxing hour of the dancer..
+	}
+	
+}
+class Font
+{
+	void setFont() {
+		System.out.println("Setting the font....");
+	}
+}
+class Singer {
+	void sing() {
+		System.out.println("Signer is singing....");
+	}
+}
+class Writer {
+	void write() {
+		System.out.println("Writer is writing....");
 	}
 }
 class Boxer
