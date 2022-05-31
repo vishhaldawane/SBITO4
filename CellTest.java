81f68c67eb86a82a7f007ac7cb735c87f2743dc0
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2022-05-31 14:47:20 +0530

    constructor

--- CellTest.java
+++ CellTest.java
@@ -2,18 +2,12 @@
 public class CellTest {
 	public static void main(String[] args) {
 
-		Cell cell1 = new Cell();
-		Cell cell2 = new Cell();
-		Cell cell3 = new Cell();
-		Cell cell4 = new Cell();
-		Cell cell5 = new Cell();
-		
-		cell1.setCell("A",1,"50");
-		cell2.setCell("A",2,"60");
-		cell3.setCell("A",3,"70");
-		cell4.setCell("A",4,"80");
-		cell5.setCell("A",5,"90");
-		
+		Cell cell1 = new Cell("A",1,"50");
+		Cell cell2 = new Cell("A",2,"60");
+		Cell cell3 = new Cell("A",3,"70");
+		Cell cell4 = new Cell("A",4,"80");
+		Cell cell5 = new Cell("A",5,"90");
+
 		cell1.printCell();
 		cell2.printCell();
 		cell3.printCell();
@@ -27,6 +21,12 @@ class Cell
 	String columnName;
 	int rowNumber;
 	String cellValue;
+	
+	Cell(String col, int row, String value) {
+		columnName = col;
+		rowNumber = row;
+		cellValue = value;
+	}
 	
 	void setCell(String col, int row, String value) {
 		columnName = col;
