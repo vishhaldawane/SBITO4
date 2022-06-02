eea2f7c6c241607c605a8c58db8d3b7d100a4eed
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2022-06-02 12:26:29 +0530

    Add files via upload

--- KiteTest.java
+++ KiteTest.java
@@ -7,10 +7,24 @@ public class KiteTest {
 		Kite k2 = new Kite("White",60,"Nandish");
 		Kite k3 = new Kite("Black",70,"Karan");
 		
+		System.out.println("k1 "+k1);
+		System.out.println("k2 "+k2);
+		System.out.println("k3 "+k3);
+		
 		Kite.printKiteCount();
+		
+		Kite.printKiteCount();
+		k1.kiteFight(k2);
+		
+		Kite.printKiteCount();
+		
+		System.out.println("k1 "+k1);
+		System.out.println("k2 "+k2);
+		System.out.println("k3 "+k3);
+		
+		System.out.println("End of main..");
 	}
 }
-
 class Kite
 {
 	//below member is not belong to the object
@@ -20,6 +34,35 @@ class Kite
 	private        String color;
 	private        int    length;
 	private        String owner;
+	private       boolean flyingStatus;
+	
+	void kiteFight(Kite x) {
+		System.out.println(color+" color kite initiated kite fight with "+x.color+" color kite");
+		
+		for(int i=1;i<=20;i++) {
+			double v = Math.random()%10; // 0 to 1
+			System.out.println(i+" v "+v);
+			if(v > 0.98) {
+				flyingStatus=false;
+				kiteCount--;
+				System.out.println(color+" kite is down...");
+				break;
+			}
+			else if(v < 0.01) {
+				x.flyingStatus=false;
+				kiteCount--;
+				System.out.println(x.color+" kite is down...");
+				break;
+			} 
+			else if ( v >= 0.60 && v<=0.70) {
+				flyingStatus=false;
+				x.flyingStatus=false;
+				System.out.println("Both kites are down...");
+				kiteCount= kiteCount-2;
+				break;
+			}
+		}
+	}
 	
 	public Kite(String color, int length, String owner) {
 		super();
@@ -28,10 +71,17 @@ class Kite
 		this.length = length;
 		this.owner = owner;
 		++kiteCount;
+		flyingStatus=true;
 	}
 
 	public static void printKiteCount() {
 		System.out.println("Kites in the sky : "+kiteCount);
+	}
+
+	@Override
+	public String toString() {
+		return "Kite [color=" + color + ", length=" + length + ", owner=" + owner + ", flyingStatus=" + flyingStatus
+				+ "]";
 	}
 	
 }
