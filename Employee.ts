9b4d5b5389b7ad7650b9c1b09aa35b0ae22b8c78
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2022-06-14 12:17:45 +0530

    angular

---
+++ Employee.ts
@@ -0,0 +1,10 @@
+export class Employee {
+    employeeNumber!: number;
+    empName!:        string;
+    job!:            string;
+    managerCode!:    number;
+    hireDate!:       Date;
+    basicSalary!:    number;
+    commission!:     number;
+    deptNumber!:     number;
+}
