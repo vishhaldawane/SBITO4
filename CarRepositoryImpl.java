392123c99a3f761e698dd06199b88d8d8e475945
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2022-06-18 13:03:19 +0530

    Add files via upload

--- CarRepositoryImpl.java
+++ CarRepositoryImpl.java
@@ -6,28 +6,31 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository("carRepo")
 public class CarRepositoryImpl implements CarRepository {
 
+	@PersistenceContext(name = "MyJPA")
 	EntityManager entityManager;
 	
 	public CarRepositoryImpl() {
-		EntityManagerFactory entityManagerFactory  = Persistence.createEntityManagerFactory("MyJPA");
-		System.out.println("Entity manager factory : "+entityManagerFactory);
-		entityManager = entityManagerFactory.createEntityManager();	
-		System.out.println("Entity manager         : "+entityManager);
+		//EntityManagerFactory entityManagerFactory  = Persistence.createEntityManagerFactory("MyJPA");
+		//System.out.println("Entity manager factory : "+entityManagerFactory);
+		//entityManager = entityManagerFactory.createEntityManager();	
+		//System.out.println("Entity manager         : "+entityManager);
 	}
 	
-	@Override
+	@Transactional
 	public void insertCar(Car carObj) {
-		EntityTransaction et = entityManager.getTransaction();
+		//EntityTransaction et = entityManager.getTransaction();
 		System.out.println("Entity transaction     : "+et);
-		et.begin();
+	//	et.begin();
 			entityManager.persist(carObj);
-		et.commit();
+	//	et.commit();
 	}
 
 	@Override
