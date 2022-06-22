89e2f096d0c1d620586b737942a7d44cb4629ce4
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2022-06-22 14:26:30 +0530

    updated

--- FlightController.java
+++ FlightController.java
@@ -3,6 +3,7 @@ package com.example.demo.layer5;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,12 @@ import com.example.demo.exceptions.FlightNotFoundException;
 import com.example.demo.layer2.Flight;
 import com.example.demo.layer4.FlightService;
 
+
+// http://localhost:8080
+//					|
+// http://localhost:4200
+
+@CrossOrigin			//CORS
 @RestController
 @RequestMapping("/flight")
 public class FlightController {
@@ -29,13 +36,13 @@ public class FlightController {
 			responseRef = ResponseEntity.ok(flight);
 			return responseRef;
 		} catch (FlightNotFoundException e) {
-			//throw e;
-			System.out.println("Inside flight not found exception ");
-			ResponseStatus respStatus = new ResponseStatus();
-			respStatus.setMessage(e.getMessage());
-			responseRef = ResponseEntity.status(HttpStatus.NOT_FOUND).body(respStatus);
-			//return responseRef;
+			throw e;
+			//System.out.println("Inside flight not found exception ");
+			//ResponseStatus respStatus = new ResponseStatus();
+			//respStatus.setMessage(e.getMessage());
+			//responseRef = ResponseEntity.status(HttpStatus.NOT_FOUND).body(respStatus);
+			//return ;
 		}
-		return responseRef;
+		//return responseRef;
 	}
 }
--- FlightNotFoundAdvice.java
+++ FlightNotFoundAdvice.java
@@ -1,6 +1,10 @@
 package com.example.demo.exceptions;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,9 +16,12 @@ public class FlightNotFoundAdvice {
 	@ResponseBody
 	@ExceptionHandler(FlightNotFoundException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)
-	String flightNotFoundHandler(FlightNotFoundException ex)
+	public ResponseEntity<?> flightNotFoundHandler(FlightNotFoundException ex)
 	{
 		System.out.println("flightNotFoundHandler.....");
-		return ex.getMessage() + " its not there, means 404";
+		//return ex.getMessage() + " its not there, means 404";
+		Map<String,String> errors = new HashMap<String,String>();
+		errors.put("message", ex.getMessage());
+		return new ResponseEntity<>(errors, HttpStatus.NOT_FOUND);
 	}
 }
