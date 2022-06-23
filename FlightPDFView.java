515dcf3f8f88a426bb916e6f5f41a3fc4d6ad9ac
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2022-06-23 17:39:22 +0530

    pdf new

--- FlightPDFView.java
+++ FlightPDFView.java
@@ -1,5 +1,5 @@
 package com.example.demo.pdf;
-
+import com.itextpdf.text.BaseColor;
 import java.awt.Point;
 import java.util.HashMap;
 import java.util.List;
@@ -14,13 +14,18 @@ import org.springframework.web.servlet.view.document.AbstractPdfView;
 
 import com.example.demo.layer2.Flight;
 import com.example.demo.layer4.FlightService;
-import com.lowagie.text.Document;
-import com.lowagie.text.Paragraph;
-import com.lowagie.text.Table;
-import com.lowagie.text.pdf.PdfWriter;
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
 
 @Component
-public class FlightPDFView  extends AbstractPdfView {
+public class FlightPDFView  extends AbstractITextPdfView  {
 
 	@Autowired
 	FlightService flightService;
@@ -29,7 +34,7 @@ public class FlightPDFView  extends AbstractPdfView {
 	protected void buildPdfDocument(
 			Map<String, 
 			Object> model, 
-			Document document, 
+			Document doc, 
 			PdfWriter writer,
 			HttpServletRequest request, 
 			HttpServletResponse response) throws Exception {
@@ -37,31 +42,54 @@ public class FlightPDFView  extends AbstractPdfView {
 		
 		System.out.println("buildPdfDocument(..) invoked...");
 		
-		
-		
-		Table table = new Table(2);
-		
-		table.addCell("FLNO");
-		table.addCell("AIRLINE");
-		/*table.addCell("SOURCE");
-		table.addCell("TARGET");
-		table.addCell("DEPARTURE");
-		table.addCell("ARRIVAL");
-		*/
-		
 		List<Flight> allFlights =  flightService.findAllFlightsService();
 		
-		Map<Integer,String> map = new HashMap();
-		
-		
-		for (Flight flight : allFlights) {
-			map.put(Integer.valueOf(flight.getFlightNumber()), flight.getAirline());
-		}
-		for (Map.Entry<Integer, String> entry : map.entrySet()) {
-			table.addCell(entry.getKey()+"");
-			table.addCell(entry.getValue());
-        }
-		document.add(table);
+		 doc.add(new Paragraph("Flight Details"));
+         
+	        PdfPTable table = new PdfPTable(5);
+	        table.setWidthPercentage(100.0f);
+	        table.setWidths(new float[] {3.0f, 2.0f, 2.0f, 2.0f, 1.0f});
+	        table.setSpacingBefore(10);
+	         
+	        // define font for table header row
+	        Font font = FontFactory.getFont(FontFactory.HELVETICA);
+	        font.setColor(BaseColor.WHITE);
+	         
+	        // define table header cell
+	        PdfPCell cell = new PdfPCell();
+	        cell.setBackgroundColor(BaseColor.BLUE);
+	        cell.setPadding(5);
+	         
+	        // write table header
+	        cell.setPhrase(new Phrase("Flight Number", font));
+	        table.addCell(cell);
+	         
+	        cell.setPhrase(new Phrase("Airline", font));
+	        table.addCell(cell);
+	 
+	        cell.setPhrase(new Phrase("Source", font));
+	        table.addCell(cell);
+	         
+	        cell.setPhrase(new Phrase("Destination", font));
+	        table.addCell(cell);
+	         
+	        cell.setPhrase(new Phrase("Departure", font));
+	        table.addCell(cell);
+	        
+	        //cell.setPhrase(new Phrase("Arrival", font));
+	       // table.addCell(cell);
+	         
+	        // write table row data
+	        for (Flight flight: allFlights) {
+	            table.addCell(flight.getFlightNumber()+"");
+	            table.addCell(flight.getAirline());
+	            table.addCell(flight.getSourceCity());
+	            table.addCell(flight.getTargetCity());
+	            table.addCell(flight.getFlightDepartureTime()+"");
+	          //  table.addCell(flight.getFlightArrivalTime()+"");
+	        }
+	         
+	        doc.add(table);
 	}
 
 	
